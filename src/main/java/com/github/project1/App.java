ebc1bca27f7216d346d1583d118231d21ccc1e51
JohnZSuh <john.z.suh@gmail.com>
2022-09-02 22:06:28 -0500

    created basic app that allows for connection to postman

---
+++ src/main/java/com/github/project1/App.java
@@ -0,0 +1,35 @@
+package com.github.project1;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+import com.github.project1.auth.AuthServlet;
+import com.github.project1.users.UserDAO;
+import com.github.project1.users.UserServlet;
+
+public class App {
+    public static void main(String[] args) throws LifecycleException { 
+
+        String docBase = System.getProperty("java.io.tmpdir");
+        Tomcat webServer = new Tomcat();
+        
+        webServer.setBaseDir(docBase);
+        webServer.setPort(5000);
+        webServer.getConnector();
+
+        UserDAO userDAO = new UserDAO();
+        UserServlet userServlet = new UserServlet(userDAO);
+        AuthServlet authServlet = new AuthServlet(userDAO);
+
+
+        final String rootContext = "/p0";
+        webServer.addContext(rootContext, docBase);
+        webServer.addServlet(rootContext, "UserServlet", userServlet).addMapping("/users");
+        webServer.addServlet(rootContext, "AuthServlet", authServlet).addMapping("/auth");
+
+
+        webServer.start();
+        webServer.getServer().await();
+        System.out.println("web application successfully started.");
+    }
+}
