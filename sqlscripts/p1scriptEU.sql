0dd7a25814debea0902f486bc4f173e27025432b
JohnZSuh <john.z.suh@gmail.com>
2022-09-08 13:58:27 -0500

    "deleting hard to read java filing"

--- sqlscripts/p1scriptEU.sql
+++ sqlscripts/p1scriptEU.sql
@@ -13,3 +13,14 @@ CREATE TABLE ers_users (
 ALTER TABLE ers_users
 ADD CONSTRAINT FK_UserRoles
 FOREIGN KEY (role_id) REFERENCES ers_user_roles (role_id);
+
+ALTER TABLE ers_users 
+DROP COLUMN role_id;
+
+ALTER TABLE ers_users 
+ADD COLUMN role_id uuid;
+
+SELECT * FROM ers_users;
+
+INSERT INTO ers_users (user_id, username, email, PASSWORD, given_name, surname, is_active, role_id)
+VALUES ()
--- sqlscripts/p1scriptEUR.sql
+++ sqlscripts/p1scriptEUR.sql
@@ -6,3 +6,26 @@ CREATE TABLE ers_user_roles (
 
 ALTER TABLE ers_user_roles
 ADD UNIQUE(role);
+
+SELECT * FROM ers_user_roles;
+
+ALTER TABLE ers_user_roles
+ALTER role
+SET DEFAULT 'EMPLOYEE';
+
+ALTER TABLE ers_user_roles 
+ALTER role_id
+SET DEFAULT gen_random_uuid();
+
+ALTER TABLE ers_user_roles 
+ALTER role_id TYPE uuid
+USING role_id::uuid;
+
+INSERT INTO ers_user_roles (role)
+VALUES ('ADMIN');
+
+INSERT INTO ers_user_roles (role)
+VALUES ('FINANCE MANAGER');
+
+INSERT INTO ers_user_roles (role)
+VALUES ('EMPLOYEE');
